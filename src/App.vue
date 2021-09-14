1d1475a51481c03ff842b81d1b2e59eaf8d3620b
SelimReza1 <selimreza9920@gmail.com>
2021-09-14 17:37:18 +0600

    conditional rendering

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,16 @@
 <template>
-<div>{{greet}} {{name}}</div>
-  <div v-html="channel"></div>
-  <div :id="headingId">Heading</div>
-  <button :disabled="isDisabled">Bind</button>
-  <h2 class="underline">underline</h2>
-  <h2 class="underline" :class="status">Status</h2>
-  <h2 :class="isPromoted && 'promoted'">Promoted Movie</h2>
-  <h2 :class="isSoldout? 'soldout' : 'new'">Soldout? Movie</h2>
-  <h2 :class="['new', 'promoted']">Newly Promoted Movie</h2>
-  <h2 :class="[isPromoted && 'promoted', isSoldout? 'soldout' : 'new']">Array Conditional Movie</h2>
-  <h2 :class="{
-                promoted: isPromoted,
-                new : !isSoldout,
-                soldout: isSoldout
-              }">Object Conditional Movie</h2>
-  <h2 :style="{
-    color: highlightColor,
-    fontSize: headerSize + 'px',
-    padding: '20px'
-}">Inline Style</h2>
-  <h2 :style="headerStyleObject">Style Object</h2>
-  <h2 :style="[baseStyleObject, successStyleObject]">Success Style</h2>
-  <h2 :style="[baseStyleObject, dangerStyleObject]">Danger Style</h2>
+  <h2 v-if="num === 0">The number is zero</h2>
+  <h2 v-else-if="num > 0">The number is positive</h2>
+  <h2 v-else-if="num < 0">The number is negative</h2>
+  <h2 v-else>Not a number</h2>
+
+  <template v-if="display">
+  <h2>Selim</h2>
+  <h2>Vue</h2>
+  </template>
+
+  <h2 v-if="display">Display by v-if</h2>
+  <h2 v-show="display">Display by v-show</h2>
 </template>
 
 <script>
@@ -30,36 +19,8 @@ export default {
   name: 'App',
   data() {
     return{
-      greet:"Hello",
-      name:"Selim",
-      channel:"<b>Component</b>",
-      headingId: "heading",
-      isDisabled: false,
-      status: 'success',
-      isPromoted: false,
-      isSoldout: true,
-      highlightColor: 'orange',
-      headerSize: 50,
-      headerStyleObject: {
-        color: 'orange',
-        fontSize: '50px',
-        padding: '20px',
-      },
-      baseStyleObject: {
-        fontSize: '50px',
-        padding: '10px',
-      },
-      successStyleObject: {
-        color: 'green',
-        background: 'lightgreen',
-        border: '1px solid green',
-        padding: '20px',
-      },
-      dangerStyleObject: {
-        color: 'dark',
-        background: 'red',
-        border: '1px solid dark',
-      },
+     num: 0,
+      display: false
     }
   }
 }
