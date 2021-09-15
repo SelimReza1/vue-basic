1cc0c519b5082eb7bc1cdf77b08599115c8c8353
SelimReza1 <selimreza9920@gmail.com>
2021-09-15 22:24:34 +0600

    computed properties and v-for

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,10 @@
   <h2>Computed Total - {{total}}</h2>
   <h2>Method Total - {{getTotal()}}</h2>
   <input type="text" v-model="country">
+  <template v-for="item in items" :key="item.id">
+    <h2 v-if="item.price >100">{{item.title}} {{item.price}}</h2>
+  </template>
+  <h2 v-for="item in expensiveItems" :key="item.id">{{item.title}} {{item.price}}</h2>
 </template>
 
 <script>
@@ -50,6 +54,9 @@ export default {
       console.log('total computed property');
       return this.items.reduce((total, curr) =>(total = total + curr.price), 0);
     },
+    expensiveItems(){
+      return this.items.filter(item => item.price > 100);
+    }
   },
 }
 </script>
