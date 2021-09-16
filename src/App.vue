91dbacaacbd76b51651bffbd2ec774c56a6c01b0
SelimReza1 <selimreza9920@gmail.com>
2021-09-16 10:31:55 +0600

    watchers

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,10 @@
 <template>
- <h2>Full Name - {{firstName}} {{lastName}}</h2>
-<h2>Computed full name {{fullName}}</h2>
-  <button @click="changeFullName">Change FullName</button>
-  <button @click="items.push({id:4,title:'keyboard',price:50})">Add Item</button>
-  <h2>Computed Total - {{total}}</h2>
-  <h2>Method Total - {{getTotal()}}</h2>
-  <input type="text" v-model="country">
-  <template v-for="item in items" :key="item.id">
-    <h2 v-if="item.price >100">{{item.title}} {{item.price}}</h2>
-  </template>
-  <h2 v-for="item in expensiveItems" :key="item.id">{{item.title}} {{item.price}}</h2>
+<h2>Volume Tracker (0-20)</h2>
+  <h2>Curent Volume {{volume}}</h2>
+  <div>
+    <button @click="volume +=2">Increase</button>
+    <button @click="volume -=2">Decrease</button>
+  </div>
 </template>
 
 <script>
@@ -18,57 +13,22 @@ export default {
   name: 'App',
   data() {
     return{
-    firstName: 'Selim',
-      lastName: 'Hossain',
-      items: [
-        {
-          id: 1,
-          title: 'TV',
-          price: 100,
-        },
-        {
-          id: 2,
-          title: 'Phone',
-          price: 200,
-        },
-        {
-          id: 3,
-          title: 'Laptop',
-          price: 300,
-        },
-      ],
-      country:''
+        volume: 0,
     }
   },
   methods: {
-    getTotal(){
-      console.log('get total method');
-      return this.items.reduce((total, curr) =>(total = total + curr.price), 0);
-    },
-    changeFullName(){
-      this.fullName = 'Mikel Clark';
-    }
+
   },
   computed: {
-    fullName: {
-      get(){
-        return this.firstName +' '+ this.lastName;
-      },
-      set(value){
-        const names = value.split(' ');
-        this.firstName = names[0],
-            this.lastName = names[1]
-      }
 
-    },
-    total(){
-      console.log('total computed property');
-      return this.items.reduce((total, curr) =>(total = total + curr.price), 0);
-    },
-    expensiveItems(){
-      return this.items.filter(item => item.price > 100);
-    }
   },
+  watch:{
+    volume(newValue, oldValue){
+      if(newValue > oldValue && newValue === 16){
+        alert('Listening to a high volume for a long time may damage your hearing');
+      }
+    }
+  }
 }
 </script>
 
