18b92595767719c55e8bd177488c0bb5a3145563
SelimReza1 <selimreza9920@gmail.com>
2021-09-16 11:22:06 +0600

    Immediate and Deep Watchers

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,10 @@
     <button @click="volume +=2">Increase</button>
     <button @click="volume -=2">Decrease</button>
   </div>
+  <input type="text" v-model="movie">
+  <input type="text" v-model="movieInfo.title">
+  <input type="text" v-model="movieInfo.actor">
+  <button @click="movieList = movieList.concat(['Wonder Woman'])">Add Movie</button>
 </template>
 
 <script>
@@ -14,6 +18,12 @@ export default {
   data() {
     return{
         volume: 0,
+      movie: 'Batman',
+      movieInfo: {
+          title: '',
+          actor: '',
+      },
+      movieList: ['Batman', 'Superman'],
     }
   },
   methods: {
@@ -27,8 +37,27 @@ export default {
       if(newValue > oldValue && newValue === 16){
         alert('Listening to a high volume for a long time may damage your hearing');
       }
+    },
+    movie: {
+      handler(newValue){
+        console.log('Calling Api with movie name=' + newValue);
+      },
+      immediate: true,
+    },
+    movieInfo: {
+      handler(newValue){
+        console.log('Calling Api with movie title= '+ newValue.title + ' and actor= '+ newValue.actor);
+      },
+      deep: true,
+    },
+    movieList:{
+      handler(newValue){
+        console.log('Updated List '+ newValue);
+      },
+      // deep: true,
     }
-  }
+
+    },
 }
 </script>
 
